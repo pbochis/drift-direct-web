da58c780afcd0b612869540a1cc2066eb1a901a4
Paul Bochis <paul.bochis@gmail.com>
2016-01-21 21:31:40 +0200

    Revert "publish vulcanized version"
    This reverts commit 60bd1f403f3d405c1911b77b915f33fc523d4736.

--- scripts/util.js
+++ scripts/util.js
@@ -1,1 +1,75 @@
-var util={build:function(e){var r="http://localhost:8080";return r+e},imageLink:function(e,r,t){return e?void 0!==r&&void 0!==t?this.build("/file/"+e+"?height="+t+"&width="+r):this.build("/file/"+e):"/images/user.png"},loggedIn:function(){return null!=localStorage.getItem("user")?JSON.parse(localStorage.getItem("user")):void 0},currentUser:function(){return JSON.parse(localStorage.getItem("user"))},hasRole:function(e){for(var r=JSON.parse(localStorage.getItem("user")),t=0;t<r.roles.length;t++)if(r.roles[t]===e)return!0;return!1},isJudge:function(){for(var e=JSON.parse(localStorage.getItem("user")),r=0;r<e.roles.length;r++)if("ROLE_JUDGE"===e.roles[r])return!0;return!1},isOrganizer:function(){for(var e=JSON.parse(localStorage.getItem("user")),r=0;r<e.roles.length;r++)if("ROLE_ORGANIZER"===e.roles[r])return!0;return!1},formatDate:function(e,r){var t=Number(e.getMonth())+1;console.log(t);var o=e.getDate()+"-"+t+"-"+e.getFullYear();return r||(o=o+" "+this.getTimeField(e.getHours())+":"+this.getTimeField(e.getMinutes())),o},getTimeField:function(e){return 10>e?"0"+e:e}};
+/**
+ * Created by Paul on 12/27/2015.
+ */
+var util = {
+	build: function(suffix){
+		var prefix = "http://46.101.120.89:8080/driftdirect";
+		//var prefix = "http://localhost:8080";
+		return prefix + suffix;
+	},
+	imageLink: function(id, width, height){
+		if (!id){
+			return '/drift-direct-web/images/user.png';
+		}
+		if (width !== undefined && height !== undefined){
+			return this.build('/file/' + id + '?height=' + height + '&width=' + width);
+		}
+		return this.build('/file/' + id);
+	},
+	loggedIn: function(){
+		if(localStorage.getItem('user') != null){
+			return JSON.parse(localStorage.getItem('user'));
+		}
+	},
+
+	currentUser: function(){
+		return JSON.parse(localStorage.getItem('user'));
+	},
+
+	hasRole: function(role){
+		var user = JSON.parse(localStorage.getItem('user'));
+		for (var i = 0; i < user.roles.length; i++){
+			if (user.roles[i] === role){
+				return true;
+			}
+		}
+		return false;
+	},
+
+	isJudge: function(){
+		var user = JSON.parse(localStorage.getItem('user'));
+		for (var i = 0; i < user.roles.length; i++){
+			if (user.roles[i] === 'ROLE_JUDGE'){
+				return true;
+			}
+		}
+		return false;
+	},
+
+	isOrganizer: function(){
+		var user = JSON.parse(localStorage.getItem('user'));
+		for (var i = 0; i < user.roles.length; i++){
+			if (user.roles[i] === 'ROLE_ORGANIZER'){
+				return true;
+			}
+		}
+		return false;
+	},
+
+	formatDate: function(date, ommitTime){
+		var month = Number(date.getMonth()) + 1;
+		console.log(month);
+		var formattedDate = date.getDate() + '-' + month + '-' + date.getFullYear();
+		if (!ommitTime){
+			formattedDate = formattedDate + ' ' + this.getTimeField(date.getHours()) + ':' + this.getTimeField(date.getMinutes())
+		}
+		return formattedDate;
+	},
+	getTimeField: function(timeField){
+		if (timeField < 10){
+			return '0' + timeField;
+		}
+		return timeField;
+
+	}
+};
